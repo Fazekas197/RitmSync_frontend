ce70a978d0acc56d052a42928906dbd2a6127dac
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-16 20:16:10 +0300

    chore: add header
    add primary color in tailwind
    change default text color
    change avatar bg color and icon color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,17 @@
 <template>
-	<header>Header</header>
+	<header>
+		<div
+			class="flex justify-between items-center px-8 py-5 border-b-2 border-black/25"
+		>
+			<h1 class="text-2xl">Ritm Sync</h1>
+			<div>
+				<UAvatar
+					icon="i-material-symbols-person"
+					size="2xl"
+					class="bg-primary"
+				/>
+			</div>
+		</div>
+	</header>
 	<slot></slot>
 </template>
