28cb001714fc00579847786f85e14b4e6baa3f27
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-17 00:25:37 +0300

    refactor: header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,14 @@
 <template>
-	<header>
-		<div
-			class="flex justify-between items-center px-8 py-5 border-b-2 border-black/25"
-		>
-			<h1 class="text-2xl">Ritm Sync</h1>
-			<div>
-				<UAvatar
-					icon="i-material-symbols-person"
-					size="2xl"
-					class="bg-primary"
-				/>
-			</div>
+	<header
+		class="flex justify-between items-center px-8 py-5 border-b-2 border-black/25"
+	>
+		<h1 class="text-2xl">Ritm Sync</h1>
+		<div>
+			<UAvatar
+				icon="i-material-symbols-person"
+				size="2xl"
+				class="bg-primary"
+			/>
 		</div>
 	</header>
 
