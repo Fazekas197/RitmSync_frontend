3c8d0e3ab5320f280a0fbc25d862a082b493ebcc
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-17 14:45:48 +0300

    chore: add filter and sort UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-	<h1 class="text-4xl">Colaborări</h1>
+	<div class="flex flex-col gap-y-2">
+		<h1 class="text-4xl">Colaborări</h1>
+		<div class="flex gap-x-2">
+			<div
+				class="flex items-center gap-x-1 border-2 rounded-xl border-primary px-3 py-1"
+			>
+				<UIcon name="i-mage:filter" class="size-7 -z-10" />
+				<h2 class="text-2xl">Filtrează</h2>
+			</div>
+			<div
+				class="flex items-center gap-x-1 border-2 rounded-xl border-primary px-3 py-1"
+			>
+				<UIcon name="i-flowbite:sort-outline" class="size-7 -z-10" />
+				<h2 class="text-2xl">Sortează</h2>
+			</div>
+		</div>
+	</div>
 	<PostCard />
 	<PostCard musician="true" />
 </template>
