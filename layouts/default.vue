6da2fa31cd0a55ec0f0d08879699e3a5f0cf9114
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-17 17:33:27 +0300

    refactor: change title

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 	<header
 		class="bg-white flex justify-between items-center fixed w-full top-0 px-8 py-5 border-b-2 border-black/25"
 	>
-		<h1 class="text-2xl">Ritm Sync</h1>
+		<h1 class="text-2xl font-[Tiny5]">RTMSNC</h1>
 		<div>
 			<UAvatar
 				icon="i-material-symbols-person"
