c031110673d054bc8bdf53ab0938b7087dd952ec
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-18 12:11:17 +0300

    refactor: make header separate component

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+	<header
+		class="bg-white flex justify-between items-center fixed w-full top-0 px-8 py-5 border-b-2 border-black/25"
+	>
+		<h1 class="text-2xl font-[Tiny5]">RTMSNC</h1>
+		<div>
+			<UAvatar
+				icon="i-material-symbols-person"
+				size="2xl"
+				class="bg-primary"
+			/>
+		</div>
+	</header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,5 @@
 <template>
-	<header
-		class="bg-white flex justify-between items-center fixed w-full top-0 px-8 py-5 border-b-2 border-black/25"
-	>
-		<h1 class="text-2xl font-[Tiny5]">RTMSNC</h1>
-		<div>
-			<UAvatar
-				icon="i-material-symbols-person"
-				size="2xl"
-				class="bg-primary"
-			/>
-		</div>
-	</header>
+	<Header />
 
 	<div class="px-8 py-7 pb-32 pt-28 flex gap-y-4 flex-col">
 		<slot></slot>
