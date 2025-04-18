3f05ed5abf07471db8fce96fcce2fbe0efa9470d
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-18 12:12:14 +0300

    refactor: make footer separate component

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+	<footer
+		class="bg-white fixed bottom-0 w-full flex justify-around py-4 shadow-black/25 shadow-[0px_-8px_80px_5px] rounded-t-4xl"
+	>
+		<ULink to="/">
+			<UIcon name="i-material-symbols-search-rounded" class="size-10" />
+		</ULink>
+
+		<ULink to="/muzicieni">
+			<UIcon name="i-pepicons-pencil-people" class="size-10" />
+		</ULink>
+
+		<ULink to="adauga">
+			<UIcon name="i-material-symbols-add-2-rounded" class="size-10" />
+		</ULink>
+	</footer>
+</template>
