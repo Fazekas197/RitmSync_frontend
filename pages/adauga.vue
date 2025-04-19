7477d31ec89660ed789d3e7dff72a59f0f6a93e2
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-19 14:24:11 +0300

    feat: add create post page

--- pages/adauga.vue
+++ pages/adauga.vue
@@ -1,10 +1,39 @@
 <template>
 	<div class="space-y-6">
 		<h1 class="text-4xl">Creaza postare</h1>
-		<div class="space-y-3">
-			<UFormField label="Email" name="email" required>
+		<form class="space-y-3">
+			<UFormField label="Titlu" required>
 				<UInput highlight />
 			</UFormField>
-		</div>
+			<div class="flex gap-x-3">
+				<UFormField label="Județ" required class="w-full">
+					<USelectMenu highlight class="w-full" />
+				</UFormField>
+				<UFormField label="Instrumente" required class="w-full">
+					<USelectMenu multiple highlight class="w-full" />
+				</UFormField>
+			</div>
+			<UFormField label="Genuri muzicale" required>
+				<USelectMenu multiple highlight class="w-full" />
+			</UFormField>
+			<UFormField label="Descriere" required>
+				<UTextarea class="w-full" highlight :rows="10" />
+			</UFormField>
+			<UFormField label="Telefon">
+				<UInput highlight />
+			</UFormField>
+			<UFormField label="Email" name="email">
+				<UInput highlight />
+			</UFormField>
+			<UFormField label="Socials">
+				<div class="flex justify-between gap-6">
+					<UInput highlight />
+					<UButton size="lg">Adaugă</UButton>
+				</div>
+			</UFormField>
+			<UButton type="submit" class="text-xl"> Crează </UButton>
+		</form>
 	</div>
 </template>
+
+<script setup lang="ts"></script>
