79ffcf72b7e1a52f14d3e7a72b2c1854a739a061
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-20 17:11:24 +0300

    chore: add filter and sort components in musicians page

--- pages/muzicieni/index.vue
+++ pages/muzicieni/index.vue
@@ -2,18 +2,8 @@
 	<div class="space-y-2">
 		<h1 class="text-4xl">Muzicieni</h1>
 		<div class="flex gap-x-2">
-			<div
-				class="flex items-center gap-x-1 border-2 rounded-xl border-primary px-3 py-1"
-			>
-				<UIcon name="i-mage:filter" class="size-7 -z-10" />
-				<h2 class="text-2xl">Filtrează</h2>
-			</div>
-			<div
-				class="flex items-center gap-x-1 border-2 rounded-xl border-primary px-3 py-1"
-			>
-				<UIcon name="i-flowbite:sort-outline" class="size-7 -z-10" />
-				<h2 class="text-2xl">Sortează</h2>
-			</div>
+			<Filter />
+			<Sort />
 		</div>
 	</div>
 	<PostCard
