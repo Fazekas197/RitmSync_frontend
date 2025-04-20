a853a0b87c00ed621cbd3dc88d8fa889897512e8
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-20 17:09:23 +0300

    chore: make filter modal it's own component

---
+++ components/Filter.vue
@@ -0,0 +1,26 @@
+<template>
+	<UModal title="Filtrează">
+		<UButton
+			icon="i-mage:filter"
+			size="xl"
+			class="text-2xl text-black font-sans bg-transparent border-2 rounded-xl border-primary px-3 py-1"
+		>
+			Filtrează
+		</UButton>
+
+		<template #body>
+			<div class="text-xl space-y-2">
+				<UFormField label="Județ" class="w-full">
+					<USelectMenu highlight class="w-full" />
+				</UFormField>
+				<UFormField label="Instrumente" class="w-full">
+					<USelectMenu highlight class="w-full" />
+				</UFormField>
+				<UFormField label="Genuri Muzicale" class="w-full">
+					<USelectMenu highlight class="w-full" />
+				</UFormField>
+				<UButton class="text-lg mt-3">Filtrează</UButton>
+			</div>
+		</template>
+	</UModal>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,7 @@
 	<div class="space-y-2">
 		<h1 class="text-4xl">Colaborări</h1>
 		<div class="flex gap-x-2">
-			<UModal title="Filtrează">
-				<UButton
-					icon="i-mage:filter"
-					size="xl"
-					class="text-2xl text-black font-sans bg-transparent border-2 rounded-xl border-primary px-3 py-1"
-				>
-					Filtrează
-				</UButton>
-
-				<template #body>
-					<div class="text-xl space-y-2">
-						<UFormField label="Județ" class="w-full">
-							<USelectMenu highlight class="w-full" />
-						</UFormField>
-						<UFormField label="Instrumente" class="w-full">
-							<USelectMenu highlight class="w-full" />
-						</UFormField>
-						<UFormField label="Genuri Muzicale" class="w-full">
-							<USelectMenu highlight class="w-full" />
-						</UFormField>
-						<UButton class="text-lg mt-3">Filtrează</UButton>
-					</div>
-				</template>
-			</UModal>
+			<Filter />
 
 			<UModal title="Sortează">
 				<UButton
