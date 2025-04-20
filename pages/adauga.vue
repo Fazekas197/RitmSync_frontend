980e8bf7e9435ab9d15de4cc47ff524b72d347cf
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-20 15:25:36 +0300

    chore: store values of form in reactive object

--- pages/adauga.vue
+++ pages/adauga.vue
@@ -1,13 +1,18 @@
 <template>
 	<div class="space-y-6">
 		<h1 class="text-4xl">Creaza postare</h1>
-		<form class="space-y-3">
+		<form class="space-y-3" @submit.prevent="submit">
 			<UFormField label="Titlu" required>
-				<UInput highlight />
+				<UInput highlight v-model="state.title" />
 			</UFormField>
 			<div class="flex gap-x-3">
 				<UFormField label="Județ" required class="w-full">
-					<USelectMenu :items="items" highlight class="w-full" />
+					<USelectMenu
+						:items="items"
+						highlight
+						class="w-full"
+						v-model="state.jud"
+					/>
 				</UFormField>
 				<UFormField
 					label="Instrumente"
@@ -19,25 +24,37 @@
 						multiple
 						highlight
 						class="w-full"
+						v-model="state.instruments"
 					/>
 				</UFormField>
 			</div>
 			<UFormField label="Genuri muzicale" required>
-				<USelectMenu :items="items" multiple highlight class="w-full" />
+				<USelectMenu
+					:items="items"
+					multiple
+					highlight
+					class="w-full"
+					v-model="state.genres"
+				/>
 			</UFormField>
 			<UFormField label="Descriere" required>
-				<UTextarea class="w-full" highlight :rows="10" />
+				<UTextarea
+					class="w-full"
+					highlight
+					:rows="10"
+					v-model="state.desc"
+				/>
 			</UFormField>
 			<UFormField label="Telefon">
-				<UInput highlight />
+				<UInput highlight v-model="state.phone" />
 			</UFormField>
 			<UFormField label="Email" name="email">
-				<UInput highlight />
+				<UInput highlight v-model="state.email" />
 			</UFormField>
 			<UFormField label="Socials">
 				<div class="flex justify-between gap-6">
-					<UInput highlight />
-					<UButton size="lg">Adaugă</UButton>
+					<UInput highlight v-model="social" />
+					<UButton size="lg" @click="addSocial">Adaugă</UButton>
 				</div>
 			</UFormField>
 			<UButton type="submit" class="text-xl"> Crează </UButton>
@@ -47,4 +64,25 @@
 
 <script setup lang="ts">
 	const items = ref(["Backlog", "Todo", "In Progress", "Done"]);
+
+	const social = ref("");
+	const state = ref({
+		title: "",
+		jud: "",
+		instruments: [],
+		genres: [],
+		desc: "",
+		phone: "",
+		email: "",
+		socials: [] as string[],
+	});
+
+	function addSocial(): void {
+		state.value.socials.push(social.value);
+		social.value = "";
+	}
+
+	function submit(): void {
+		console.log(state.value);
+	}
 </script>
