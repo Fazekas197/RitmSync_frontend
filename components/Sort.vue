dedb1125896ca59fdfc03e3650e18eb3c240fd83
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-20 17:10:34 +0300

    chore: make sort modal it's own component

---
+++ components/Sort.vue
@@ -0,0 +1,20 @@
+<template>
+	<UModal title="Sortează">
+		<UButton
+			icon="i-flowbite:sort-outline"
+			size="xl"
+			class="text-2xl text-black font-sans bg-transparent border-2 rounded-xl border-primary px-3 py-1"
+		>
+			Sortează
+		</UButton>
+
+		<template #body>
+			<div class="text-xl space-y-2">
+				<UFormField label="Criteriu" class="w-full">
+					<USelectMenu highlight class="w-full" />
+				</UFormField>
+				<UButton class="text-lg mt-3">Sortează</UButton>
+			</div>
+		</template>
+	</UModal>
+</template>
