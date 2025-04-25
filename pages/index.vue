fff3c14e34387cf271b8b051a6a975cb8fd170a2
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-25 14:27:01 +0300

    chore: fetch all posts

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,34 @@
 			<Sort />
 		</div>
 	</div>
+
 	<PostCard
-		title="Titlu"
-		ins="Bass"
-		gen="Rock"
-		loc="Iasi"
-		author="Fazekas Cosmin"
+		v-for="post in posts.data.value"
+		:title="post.title"
+		:ins="post.instruments.join(', ')"
+		:gen="post.genres.join(', ')"
+		:loc="post.county"
+		:author="post.user"
 		date="15.02.2025"
-		desc="Lorem ipsum dolor sit amet consectetur. Ornare placerat fringilla semper viverra tincidunt pulvinar et diam dignissim. Eget vitae quam nulla enim commodo. Vel massa ante nibh fringilla blandit nam..."
+		:desc="post.shortDesc"
 	/>
 </template>
+
+<script lang="ts" setup>
+	interface Post {
+		id: number;
+		user: string;
+		title: string;
+		county: string;
+		desc: string;
+		shortDesc: string;
+		phone: string;
+		email: string;
+		genres: [];
+		instruments: [];
+		socials: [];
+	}
+
+	const apiUrl = useRuntimeConfig().public.API_URL;
+	const posts = await useFetch<Post[]>(`${apiUrl}/posts`);
+</script>
