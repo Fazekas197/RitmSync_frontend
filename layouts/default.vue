18b76566ddb1ee22b94d326c92919a8f1b40b7ae
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-04-26 14:45:27 +0300

    fix: wrap app in UApp component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-	<Header />
+	<UApp>
+		<Header />
 
-	<div class="px-8 py-7 pb-32 pt-28 space-y-4">
-		<slot></slot>
-	</div>
+		<div class="px-8 py-7 pb-32 pt-28 space-y-4">
+			<slot></slot>
+		</div>
 
-	<Footer />
+		<Footer />
+	</UApp>
 </template>
